2c61426458c7fc1689b763ca1226929a0c8ce737
marcelquintela <marcelquintela@yahoo.com.br>
2020-10-24 08:00:59 -0300

    Introdução do exercício 2

---
+++ R/Exercicio_02.R
@@ -0,0 +1,6 @@
+# Especializacao em Ciencias de Dados
+# Professora: Kelly Cristina M. Goncalves
+# Disciplina: Introducao a Modelagem de Incertezas
+# Aluno: Marcel Dantas de Quintela
+# LISTA DE EXERCICIO 02
+
